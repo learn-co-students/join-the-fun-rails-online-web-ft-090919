d2b9a06cb6d9e59034408d7af906b8dc4961dae6
Sarah Henderson <sarahrose.henderson@gmail.com>
2019-11-18 10:22:05 -0600

    Done.

--- db/migrate/20140212205318_create_passengers.rb
+++ db/migrate/20140212205318_create_passengers.rb
@@ -2,6 +2,8 @@ class CreatePassengers < ActiveRecord::Migration
   def change
     create_table :passengers do |t|
       t.timestamps null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
     end
   end
 end
