a38f79b2502928cbf58a5bfe0363b47d7f2b8892
averagealloy <m.mctighe10@gmail.com>
2019-11-18 14:20:10 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,6 @@
 class Ride < ActiveRecord::Base
+   belongs_to :passenger 
+   belongs_to :taxi
 end
+#a ride is shared between 2 things with out passengers a ride doesnt exist and if you have passengers and nothing to take them in the ride cant exist
+
--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,5 @@
 class Taxi < ActiveRecord::Base
+ has_many :rides 
+ has_many :passengers, through: :rides 
 end
+#a taxi is a vessel that takes rides for thoes rides it uses passengers 
